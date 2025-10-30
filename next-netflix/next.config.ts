12c61feb211a28f7c7010da78f5bcc75c0586985
hammoooo <chlangjs@g.hongik.ac.kr>
2025-10-30 15:54:40 +0900

    feature: tabBar 구현

--- next-netflix/next.config.ts
+++ next-netflix/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "image.tmdb.org", pathname: "/**" },
+    ],
+  },
 };
 
 export default nextConfig;
